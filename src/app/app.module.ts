f63f7995836089628180620df391a10cb94a2b2d
PedroHMNobrega <pedrohmnobrega@gmail.com>
2022-07-31 16:46:04 -0300

    remove unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,15 @@ import { HttpClientModule } from "@angular/common/http";
 
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RepositoriesComponent } from './repositories/repositories.component';
-import { RepositoriesFiltersComponent } from './repositories-filters/repositories-filters.component'
+import { AppComponent } from './app.component'
 import { PullsFiltersComponent } from './pulls-filters/pulls-filters.component'
 import { NotFoundComponent } from './not-found/not-found.component';
-import { UserInfoComponent } from './user-info/user-info.component';
 import { PullsComponent } from './pulls/pulls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositoriesComponent,
-    RepositoriesFiltersComponent,
     NotFoundComponent,
-    UserInfoComponent,
     PullsComponent,
     PullsFiltersComponent
   ],
