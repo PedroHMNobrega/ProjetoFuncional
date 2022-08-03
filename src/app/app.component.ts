ce6ab43ad18747c06261b09634d9413ec3eee869
PedroHMNobrega <pedrohmnobrega@gmail.com>
2022-08-03 16:50:37 -0300

    change app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-example-app';
+  title = 'github-pull-requests';
 }
