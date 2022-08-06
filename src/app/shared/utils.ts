6d527a9d8b5739ee71b3ebd5a3887e598319cbfe
Mateus Tranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2022-08-06 16:57:25 -0300

    add fold and compose functions

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -11,3 +11,8 @@ export const groupBy = (items, getKeyFn) => items.reduce(
             ]
         }
     }, {})
+
+export const fold = (reducer, init, array) =>
+    array.length === 0 ? init : fold(reducer, reducer(init, array[0]), array.slice (1))
+
+export const compose = (f, g) => x => f( g(x) )
